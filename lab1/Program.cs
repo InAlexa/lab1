1ce95ff3761bcdc3ce0e99c493976ae33c8c01ec
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-07-17 09:23:36 -0600

    variables para operar los números pedidos

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -3,3 +3,11 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("SE va a joder");
 
 Console.WriteLine("A ver que pasa");
+
+
+
+
+int suma = numero1 + numero2;
+int resta = numero1 - numero2;
+int multi = numero1 * numero2;
+int div = numero1 / numero2;
