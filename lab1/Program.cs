5d3be4998d13fe92fdb067763be106eaf13ea584
Daniela Marín <danie0le2@gmail.com>
2024-07-17 09:20:20 -0600

    <cree unas variables>

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("SE va a joder");
 Console.WriteLine(":(");
+
+Console.WriteLine("Ingrese un número:");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese otro número:");
+int numero2 = int.Parse(Console.ReadLine());
