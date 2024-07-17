c720527b95f5eb50f144d5eb12e8f356d37eebb6
Daniela Marín <danie0le2@gmail.com>
2024-07-17 09:08:00 -0600

    <texto en otra linea>

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
+
+=======
+Console.WriteLine("SE va a joder");
+>>>>>>> 75b9d33b818b7ccd5b821a8b23f6a1eb8d33f8fd
+
+
+
 Console.WriteLine("XDLOLOLOLOLOLOLOLOLOLOLOLOLOL");
